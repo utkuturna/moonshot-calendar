52175b65885b5e4a4ca1de3379335a9ca3cb9a9f
Utku Turna <turnautku@gmail.com>
2022-02-12 15:27:15 +0300

    Fetch and combine all results for 3 months

--- src/api/launch.ts
+++ src/api/launch.ts
@@ -1,7 +1,19 @@
-import {getApiBase, getHeaders} from "./utils";
+import {convertFiltersIntoQuery, getApiBase, getHeaders} from "./utils";
 
-export const getLaunches = async () => {
-  const url = getApiBase() + '/launch';
+export const getLaunches = async (filters: string[]) => {
+  const url = getApiBase() + '/launch' + convertFiltersIntoQuery(filters);
+  const response = await fetch(url, {
+    method: 'GET',
+    headers: getHeaders()
+  });
+  const data = await response.json();
+  if(!response.ok) {
+    throw new Error(data.message);
+  }
+  return data;
+}
+
+export const getLaunchesWithUrl = async (url: string) => {
   const response = await fetch(url, {
     method: 'GET',
     headers: getHeaders()
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,24 +1,58 @@
-import React, {useCallback, useEffect} from 'react';
-import {getLaunches} from "../api/launch";
+import React, {useCallback, useEffect, useState} from 'react';
+import {getLaunches, getLaunchesWithUrl} from "../api/launch";
+
+const now = new Date(); // Get the time for now
+now.setMonth(now.getMonth() - 3); // Calculate three months ago
+
+const INITIAL_FILTERS = [
+  'window_start__gt=' + now.toISOString()
+]
 
 const Home: React.FC = () => {
+  const [filters, setFilters] = useState<any>(INITIAL_FILTERS);
+  const [launches, setLaunches] = useState<any>([]);
 
-
-  const fetchLaunches = useCallback(() => {
-    getLaunches().then(res => {
-      console.log(res);
-    }).catch(err => {
+  const fetchNextLaunches = useCallback((next: string) => { // Fetch next results with url
+    getLaunchesWithUrl(next).then( res => {
+      if(res.next) {
+        setLaunches((state: any) => {
+          return [...state, ...res.results]
+        })
+        fetchNextLaunches(res.next);
+      }
+    }).catch( err => {
       console.log(err);
     })
   }, []);
 
+  const fetchLaunches = useCallback(() => { // Fetch initial values with filters
+    getLaunches(filters).then(res => {
+      console.log()
+      if(res.next) {
+        fetchNextLaunches(res.next)
+      }
+      setLaunches((state: any) => {
+        return [...state, ...res.results]
+      })
+    }).catch(err => {
+      console.log(err);
+    })
+  }, [fetchNextLaunches, filters]);
+
+
+
   useEffect(() => {
+    setLaunches([]);
     fetchLaunches();
   }, [fetchLaunches]);
 
   return (
     <div>
-      HOME PAGE
+      {launches.map((launch: any) => {
+        return (
+          <div key={launch.id}>{launch.name}</div>
+        )
+      })}
     </div>
   )
 }
